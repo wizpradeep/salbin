e8e3c33511d0da58be62ee6fcc90a85fbb8ba6c9
Pradeep <wizprodeep@gmail.com>
2023-10-02 20:00:51 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// JavaScript for typewriter effect
+const textElement = document.querySelector(".myNameIsSalbin");
+const textToType = "SALBIN"; // The text you want to type out
+let charIndex = 0;
+
+function typeText() {
+  if (charIndex < textToType.length) {
+    textElement.textContent += textToType.charAt(charIndex);
+    charIndex++;
+    setTimeout(typeText, 200); // Adjust typing speed here (in milliseconds)
+  }
+}
+
+// Start typing when the page loads
+window.addEventListener("DOMContentLoaded", () => {
+  typeText();
+});
+
+const bar = document.querySelector(".fa-bars");
+const dropDown = document.querySelector(".dropDown");
+const closeBtn = document.getElementById("close");
+
+bar.addEventListener("click", () => {
+  dropDown.style.display = "block";
+  bar.style.display = "none";
+});
+
+closeBtn.addEventListener("click", () => {
+  dropDown.style.display = "none";
+  bar.style.display = "block";
+});
